1d1db24ad077426f7e15754ac7dd96eb87c5b2f3
idmp152 <ilyabelykh123@gmail.com>
2026-05-15 21:42:04 +0300

    Added text padding with side-alignment and a progressbar cell type

--- include/tree_view.hpp
+++ include/tree_view.hpp
@@ -8,6 +8,7 @@
 #define ROW_HEIGHT 30
 #define COL_HEADER_HEIGHT 30
 #define COLS 3
+#define CELL_TEXT_PADDING 10
 
 struct ViewItem {
     uint32_t node_idx;
@@ -17,17 +18,21 @@ struct ViewItem {
 class TreeView : public Fl_Table {
   Fl_Font cell_font = FL_HELVETICA;
   int cell_font_size = 16;
+  Fl_Color progressbar_color = fl_rgb_color(197, 202, 233);
 
   std::vector<ViewItem>* data = nullptr; 
 
-  void DrawHeader(const char *s, int X, int Y, int W, int H);
+  void draw_header(const char *s, int X, int Y, int W, int H);
 
-  void DrawData(const char *s, int X, int Y, int W, int H);
+  void draw_data(const char *s, int X, int Y, int W, int H, Fl_Align alignment);
+
+  void draw_progressbar(const char* s, int X, int Y, int W, int H, Fl_Align alignment, float value);
   
   void draw_cell(TableContext context, int ROW=0, int COL=0, int X=0, int Y=0, int W=0, int H=0) FL_OVERRIDE;
 
   void resize(int X, int Y, int W, int H) FL_OVERRIDE;
 public:
+  void set_progressbar_color(Fl_Color color);
   void set_font(Fl_Font font, int font_size);
   TreeView(int X, int Y, int W, int H, const char *L=0);
   ~TreeView() { }
--- src/tree_view.cpp
+++ src/tree_view.cpp
@@ -1,11 +1,17 @@
 #include "tree_view.hpp"
 #include <FL/fl_draw.H>
 
-//TODO(IlyaBelykh): Align size and percentage on the right so units of measure end up on the same side
-//TODO(IlyaBelykh): For percentage column, in CONTEXT_CELL case render a progressbar instead of just text
 const char* col_headers[COLS] = {"File", "Size", "Percentage of parent size"};
+const Fl_Align col_alignments[COLS] = {FL_ALIGN_LEFT, FL_ALIGN_RIGHT, FL_ALIGN_RIGHT}; 
 
-void TreeView::DrawHeader(const char *s, int X, int Y, int W, int H) {
+void draw_padded_text(const char *s, int X, int Y, int W, int H, Fl_Align alignment) {
+  int text_x = X + CELL_TEXT_PADDING;
+  int text_w = W - CELL_TEXT_PADDING*2;
+  fl_color(FL_GRAY0);
+  fl_draw(s, text_x,Y,text_w,H, alignment);
+}
+
+void TreeView::draw_header(const char *s, int X, int Y, int W, int H) {
     fl_push_clip(X,Y,W,H);
       fl_draw_box(FL_THIN_UP_BOX, X,Y,W,H, row_header_color());
       fl_color(FL_BLACK);
@@ -13,11 +19,30 @@ void TreeView::DrawHeader(const char *s, int X, int Y, int W, int H) {
     fl_pop_clip();
 }
 
-void TreeView::DrawData(const char *s, int X, int Y, int W, int H) { //TODO(IlyaBelykh): rename
+void TreeView::draw_data(const char *s, int X, int Y, int W, int H, Fl_Align alignment) {
     fl_push_clip(X,Y,W,H);
-      fl_color(FL_WHITE); fl_rectf(X,Y,W,H);
-      fl_color(FL_GRAY0); fl_draw(s, X,Y,W,H, FL_ALIGN_CENTER);
-      fl_color(FL_GRAY0); fl_rect(X,Y,W,H);
+      fl_color(FL_WHITE); 
+      fl_rectf(X,Y,W,H);
+
+      draw_padded_text(s, X, Y, W, H, alignment);
+
+      fl_color(FL_GRAY0);
+      fl_rect(X,Y,W,H);
+    fl_pop_clip();
+}
+
+void TreeView::draw_progressbar(const char* s, int X, int Y, int W, int H, Fl_Align alignment, float value) {
+    fl_push_clip(X,Y,W,H);
+      fl_color(FL_WHITE); 
+      fl_rectf(X,Y,W,H);
+
+      fl_color(progressbar_color);
+      fl_rectf(X, Y, W*value/100, H);
+
+      draw_padded_text(s, X, Y, W, H, alignment);
+
+      fl_color(FL_GRAY0);
+      fl_rect(X,Y,W,H);
     fl_pop_clip();
 }
 
@@ -27,10 +52,14 @@ void TreeView::draw_cell(TableContext context, int ROW, int COL, int X, int Y, i
         fl_font(cell_font, cell_font_size);
         return;
       case CONTEXT_COL_HEADER:
-        DrawHeader(col_headers[COL],X,Y,W,H);
+        draw_header(col_headers[COL],X,Y,W,H);
         return;
       case CONTEXT_CELL:
-        DrawData("0451",X,Y,W,H);
+        if (COL == 2) {
+          draw_progressbar("42 %", X, Y, W, H, col_alignments[COL], 42);
+        } else {
+          draw_data("0451",X,Y,W,H, col_alignments[COL]);
+        }
         return;
       default:
         return;
@@ -55,6 +84,11 @@ void TreeView::set_font(Fl_Font font, int font_size) {
   cell_font = font;
   cell_font_size = font_size;
   redraw(); 
+}
+
+void TreeView::set_progressbar_color(Fl_Color color) {
+  progressbar_color = color;
+  redraw();
 }
 
 TreeView::TreeView(int X, int Y, int W, int H, const char *L) : Fl_Table(X,Y,W,H,L) {
