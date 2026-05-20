73e6385a042a0752b5b42e1b47c2c5f8802de5ec
idmp152 <ilyabelykh123@gmail.com>
2026-05-20 23:58:11 +0300

    Sorted out methods for better readability

--- include/tree_view.hpp
+++ include/tree_view.hpp
@@ -21,40 +21,43 @@ enum TreeViewColumns {
 };
 
 struct ViewItem {
-    uint32_t node_idx;
-    uint32_t depth;
+    uint64_t node_idx;
+    uint64_t depth;
 };  
 
 class TreeView : public Fl_Table {
-  Fl_Font cell_font = FL_HELVETICA;
-  int cell_font_size = 16;
-  Fl_Color progressbar_color = fl_rgb_color(197, 202, 233);
-
-  std::vector<ViewItem> flat_view;
-  FileNode* root_node; 
-
-  void begin_draw_cell(int X, int Y, int W, int H);
-  void end_draw_cell(int X, int Y, int W, int H);
-
-  void draw_header(const char *s, int X, int Y, int W, int H);
-  void draw_data(const char *s, int X, int Y, int W, int H, Fl_Align alignment);
-  void draw_name(const char* s, int X, int Y, int W, int H, int depth, bool is_directory, bool is_expanded);
-  void draw_progressbar(const char* s, int X, int Y, int W, int H, Fl_Align alignment, float value);
-
-  void draw_content_cell(int ROW, int COL, int X, int Y, int W, int H);
-
-  void draw_cell(TableContext context, int ROW=0, int COL=0, int X=0, int Y=0, int W=0, int H=0) FL_OVERRIDE;
-
-  void resize(int X, int Y, int W, int H) FL_OVERRIDE;
-
-  static void table_callback(Fl_Widget* w, void* data);
-  void handle_events();
 public:
+  TreeView(int X, int Y, int W, int H, const char *L=0);
+
   void fill_flat_view(FileNode* root);
   void set_progressbar_color(Fl_Color color);
   void set_font(Fl_Font font, int font_size);
-  TreeView(int X, int Y, int W, int H, const char *L=0);
-  ~TreeView() { }
+protected:
+  // FLTK methods
+  void draw_cell(TableContext context, int ROW=0, int COL=0, int X=0, int Y=0, int W=0, int H=0) FL_OVERRIDE;
+  void resize(int X, int Y, int W, int H) FL_OVERRIDE;
+private:
+  // Configs
+  Fl_Font cell_font = FL_HELVETICA;
+  int cell_font_size = 16;
+  Fl_Color progressbar_color = fl_rgb_color(197, 202, 233);
+  
+  // Data
+  std::vector<ViewItem> flat_view;
+  FileNode* root_node; 
+  
+  // Event handling
+  static void table_callback(Fl_Widget* w, void* data);
+  void handle_events();
+
+  // Rendering
+  void draw_content_cell(int ROW, int COL, int X, int Y, int W, int H);
+  void begin_draw_cell(int X, int Y, int W, int H);
+  void end_draw_cell(int X, int Y, int W, int H);
+  void draw_data(const char *s, int X, int Y, int W, int H, Fl_Align alignment);
+  void draw_header(const char *s, int X, int Y, int W, int H);
+  void draw_name(const char* s, int X, int Y, int W, int H, int depth, bool is_directory, bool is_expanded);
+  void draw_progressbar(const char* s, int X, int Y, int W, int H, Fl_Align alignment, float value);
 };
 
 void build_flat_view(FileNode* root, std::vector<ViewItem>* flat_view, unsigned short depth);
